3b6d886bdae501302749979b29d815a7a3784e50
Ben Orrin <114030615+benorrin@users.noreply.github.com>
2024-08-18 16:00:02 +0100

    adjust venue database structure

--- BrightonVibe.Data/Entities/VenueEntity.cs
+++ BrightonVibe.Data/Entities/VenueEntity.cs
@@ -1,42 +1,51 @@
-﻿using BrightonVibe.Domain.Enums;
-
-namespace BrightonVibe.Data.Entities;
-
-using System;
+﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using BrightonVibe.Domain.Enums;
 
-public class VenueEntity
+namespace BrightonVibe.Data.Entities
 {
-    public Guid Id { get; set; }
-    
-    [Required]
-    [MaxLength(100)]
-    public string Name { get; set; }
-    
-    [Required]
-    public VenueType Type { get; set; }
-    
-    [Required]
-    [MaxLength(200)]
-    public string Address { get; set; }
-    
-    [Phone]
-    [MaxLength(15)]
-    public string? PhoneNumber { get; set; }
-    
-    [EmailAddress]
-    [MaxLength(100)]
-    public string? EmailAddress { get; set; }
-    
-    [Url]
-    [MaxLength(100)]
-    public string? Website { get; set; }
-    
-    [Url]
-    [MaxLength(100)]
-    public string? Instagram { get; set; }
-    
-    [Url]
-    [MaxLength(100)]
-    public string? Facebook { get; set; }
+    [Table("venue")]
+    public class VenueEntity
+    {
+        [Column("id")]
+        public Guid Id { get; set; }
+        
+        [Required]
+        [MaxLength(100)]
+        [Column("name")]
+        public string Name { get; set; }
+        
+        [Required]
+        [Column("type")]
+        public VenueType Type { get; set; }
+        
+        [Required]
+        [MaxLength(200)]
+        [Column("address")]
+        public string Address { get; set; }
+        
+        [Phone]
+        [MaxLength(15)]
+        [Column("phone_number")]
+        public string? PhoneNumber { get; set; }
+        
+        [EmailAddress]
+        [MaxLength(100)]
+        [Column("email_address")]
+        public string? EmailAddress { get; set; }
+        
+        [Url]
+        [MaxLength(100)]
+        [Column("website")]
+        public string? Website { get; set; }
+        
+        [MaxLength(100)]
+        [Column("instagram")]
+        public string? Instagram { get; set; }
+        
+        [MaxLength(100)]
+        [Column("facebook")]
+        public string? Facebook { get; set; }
+    }
 }
