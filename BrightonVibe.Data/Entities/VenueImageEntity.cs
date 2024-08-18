3529af20545f1a924d6b67edb5a6d15e57ce04a3
Ben Orrin <114030615+benorrin@users.noreply.github.com>
2024-08-18 16:04:23 +0100

    Adjust VenueImage database structure

--- BrightonVibe.Data/Entities/VenueImageEntity.cs
+++ BrightonVibe.Data/Entities/VenueImageEntity.cs
@@ -1,20 +1,27 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using BrightonVibe.Domain.Enums;
 
 namespace BrightonVibe.Data.Entities;
 
+[Table("venue_image")]
 public class VenueImageEntity
 {
+    [Column("id")]
     public Guid Id { get; set; }
     
     [Required]
+    [Column("venue_id")]
     public Guid VenueId { get; set; }
     
     [Url]
     [MaxLength(100)]
+    [Column("image_url")]
     public string? ImageUrl { get; set; }
     
+    [Column("featured")]
     public bool Featured { get; set; }
     
+    [Column("created_at")]
     public DateTime CreatedAt { get; set; }
 }
